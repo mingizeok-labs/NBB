8ec2e00cfb9aa62d9f06c6b754119365272affc8
KIM SHINEDAY <kimshineday99@gmail.com>
2026-01-22 16:32:14 +0900

    ✅ (Test) : 숫자야구 판정 로직 Test Case 작성 - 테스트 성공 케이스만 작성 - 추후 실패 케이스 작성 필요

--- tests/test_game.py
+++ tests/test_game.py
@@ -4,6 +4,7 @@ from pytest import raises
 
 from app.main import app
 from app.services.create import create_number
+from app.services.game import Verification
 from app.schemas.game_setting import RandomInt
 
 client = TestClient(app)
@@ -41,3 +42,25 @@ class RandomIntFailCaseTest:
     def test_randomint_fail_3():
         with raises(ValidationError):
             RandomInt(number='')
+
+# 숫자 검증 로직 test
+class TestVerification:
+    def test_success_case_1(self):
+        v = Verification(input_number='1234', answer='1234')
+        assert v.check_logic()['1234'] == '정답'
+
+    def test_success_case_2(self):
+        v = Verification(input_number='1256', answer='1234')
+        assert v.check_logic()['1256'] == '2S0B'
+
+    def test_success_case_3(self):
+        v = Verification(input_number='1243', answer='1234')
+        assert v.check_logic()['1243'] == '2S2B'
+
+    def test_success_case_4(self):
+        v = Verification(input_number='3412', answer='1234')
+        assert v.check_logic()['3412'] == '0S4B'
+    
+    def test_success_case_5(self):
+        v = Verification(input_number='0909', answer='1234')
+        assert v.check_logic()['0909'] == 'out'
