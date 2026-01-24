bbcf9f472ce62ac24de37988f2c95234e8791fed
KIM SHINEDAY <kimshineday99@gmail.com>
2026-01-24 21:00:40 +0900

    🏷️ (Schemas) : 게임진행 상황 Response Model 정의
    - 게임 진행 시, 유저가 입력한 데이터를 받아 내부 로직으로 처리 후 리턴

---
+++ app/schemas/response_model.py
@@ -0,0 +1,11 @@
+"""
+Docstring for app.schemas.response_model
+API 요청 시 반환하는 Response Model을 작성
+"""
+
+from pydantic import BaseModel
+
+class GameResultResponse(BaseModel):
+    input : dict[str, str] = {'5678':'out'}
+    count : int = 2
+    history : list[dict[str, dict[str, str]]] = [{'1': {'1234': '0S2B'}}, {'2': {'5678': 'out'}}]
