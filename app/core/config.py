494e2b61f35b98d61e070208b77cf0d8f86ee110
KIM SHINEDAY <kimshineday99@gmail.com>
2026-02-10 23:50:19 +0900

    🚀 (Setting) : CORS 추가 및 백엔드 설정 최적화 - CORS 추가 및 환경변수 설정

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,16 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from typing import Set, List
 
 class Settings(BaseSettings):
     ENV : str = 'local' # 실행환경 구분 (로컬, 테스트, 은양 베포 등 ...)
 
     SESSION_KEY : str
+    SESSION_MAX_AGE: int
+    ALLOWED_ORIGINS: Set[str]
+    USE_HTTPS: bool
+
+    ALLOWED_METHODS: List[str]
+    ALLOWED_HEADERS: List[str]
 
     model_config = SettingsConfigDict(
         env_file = '.env'
